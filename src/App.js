61bd5ab50aa29cc674700a55bf8e89ba12a5866f
Calvin Crosby <calvincrosby@Calvins-MacBook-Air.local>
2023-08-29 17:03:54 -0400

    cleanup app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
-import NavigationSidebar from "./components/navigation";
 import MovieHome from "./components/home/index.html";
-import MoviePage from "./components/movie_page";
-import movie_data from "./components/movie_page/movie.json"
-import ActorPage from "./components/actor_page";
 import MovieRouting from "./components/home/movie-routing";
 import ActorRouting from "./routing/actor-routing";
 
